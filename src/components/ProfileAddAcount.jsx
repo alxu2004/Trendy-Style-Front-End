8c025f4d3bd593aae61195c65df346d8d6858e36
Cesar Ramirez <cesaralejandroramirezzuluaga@gmail.com>
2024-03-11 19:33:15 -0500

    se agrega el formulario de usuario

--- src/components/ProfileAddAcount.jsx
+++ src/components/ProfileAddAcount.jsx
@@ -1,11 +1,98 @@
-
+import { useState } from 'react';
 import { SideBarProfile } from './SideBarProfile';
 
 export const ProfileAddAcount = () => {
+
+  const [formData, setFormData] = useState({
+    lastname: '',
+    cc: '',
+    num_cel: '',
+    city_of_residence: '',
+    address: '',
+    birthday_date:''
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formDataToSend = new FormData();
+    formDataToSend.append('lastname', formData.lastname); // Cambio en la forma de agregar el archivo
+    formDataToSend.append('cc', formData.cc);
+    formDataToSend.append('num_cel', formData.num_cel);
+    formDataToSend.append('city_of_residence', formData.city_of_residence);
+    formDataToSend.append('address', formData.address);
+    formDataToSend.append('birthday_data', formData.birthday_date);
+
+    try {
+      const response = await fetch('http://localhost:8080/api/infoUsers/registrar', {
+        method: 'POST',
+        body: formDataToSend
+      });
+      if (response.ok) {
+        alert('Producto agregado exitosamente');
+        console.log('Producto agregado exitosamente');
+      } else {
+        alert('Error al agregar producto:');
+        console.error('Error al agregar producto:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error al agregar producto:', error);
+    }
+  };
+  
+
   return (
     <div className="user-profile">
-        <SideBarProfile/>
-        
+      <SideBarProfile/>
+      <div className="profile-form">
+        <h2>Edit Profile</h2>
+          <form  onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="lastname"> segundo nombre:</label>
+              <input
+                type="text" id="lastname" name="lastname" value={formData.lastname} onChange={handleInputChange} required 
+              />
+            </div>
+            <div>
+              <label htmlFor="birthday_date"> dia de cumpleaños:</label>
+              <input
+                type="date" id="birthday_date" name="birthday_date" value={formData.birthday_date} onChange={handleInputChange} required 
+              />
+            </div>
+            <div>
+              <label htmlFor="cc"> documento de identidad:</label>
+              <input
+                type="text" id="cc" name="cc" value={formData.cc} onChange={handleInputChange} required 
+              />
+            </div>
+            <div>
+              <label htmlFor="num_cel"> numero de celular:</label>
+              <input
+                type="tel" id="num_cel" name="num_cel" value={formData.num_cel} onChange={handleInputChange} required 
+              />
+            </div>
+            <div>
+              <label htmlFor="city_of_residence"> ciudad de recidencia:</label>
+              <input
+                type="text" id="city_of_residence" name="city_of_residence" value={formData.city_of_residence} onChange={handleInputChange} required 
+              />
+            </div>
+            <div>
+              <label htmlFor="address"> direccion:</label>
+              <input
+                type="text" id="address" name="address" value={formData.address} onChange={handleInputChange} required 
+              />
+            </div>
+            <button type="submit">enviar</button>
+          </form>
+      </div>
     </div>  
   )
 }
--- src/components/ProfileEditAcount.jsx
+++ src/components/ProfileEditAcount.jsx
@@ -1,4 +1,3 @@
-import ProfileEdit from "./ProfileEdit"
 import { SideBarProfile } from "./SideBarProfile"
 
 
@@ -6,7 +5,28 @@ export const ProfileEditAcount = () => {
   return (
     <div className="user-profile">
         <SideBarProfile/>
-        <ProfileEdit/>
+        <div className="profile-form">
+      <h2>Edit Profile</h2>
+      <form >
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+          />
+        </div>
+        <button type="submit">Save</button>
+      </form>
+    </div>
     </div>  
     )
 }
