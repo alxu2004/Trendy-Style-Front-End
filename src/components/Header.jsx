464b4658f746cd49355d160e5a6a18e657556c71
Cesar Ramirez <cesaralejandroramirezzuluaga@gmail.com>
2024-03-16 16:16:02 -0500

    se terminan los estilos del header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -173,7 +173,7 @@ export const Header = ({ searchTerm, onSearchChange, onSearch,scroll }) => {
            isLoggedIn ? <>
               <Tooltip title="Open settings">
                 <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                  <Avatar alt={user?.user?.name} src="/static/images/avatar/2.jpg" />
                 </IconButton>
               </Tooltip>
               <Menu
